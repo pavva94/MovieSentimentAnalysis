168760532d47ef69fc361ddd9b984dce98d7f33a
Alessandro Pavesi <pavesialessandro@live.it>
2020-09-23 11:47:03 +0200

    Created a spark session inside main

--- src/main/scala/SentimentAnalysis.scala
+++ src/main/scala/SentimentAnalysis.scala
@@ -1,4 +1,5 @@
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
 
 
 object SentimentAnalysis {
@@ -30,6 +31,31 @@ object SentimentAnalysis {
           println("Arguments missing")
           false
       }
+    }
+
+    val spark = if (localMode) {
+      println("Local Mode selected")
+      // session for local distributed cluster
+      SparkSession.builder
+        .appName("Sentiment Analysis Classifier")
+        .master("spark://MBPdiAlessandro.homenet.telecomitalia.it:7077")
+        .getOrCreate()
+    } else {
+      println("AWS Mode selected")
+      // session for AWS
+      SparkSession.builder
+        .appName("Sentiment Analysis Classifier")
+        .getOrCreate()
+    }
+
+    if (!localMode) {
+      // AWS configuration
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3.access.key", "ASIA3XR2YR5TUZTF7BWQ")
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3.secret.key", "vK1KeTmmr41jY/ulGrg/4wZcLvTU1/kGnwJhmlku")
+      spark.sparkContext
+        .hadoopConfiguration.set("fs.s3.endpoint", "s3.amazonaws.com")
     }
 
     println("Local Mode: " + localMode)
